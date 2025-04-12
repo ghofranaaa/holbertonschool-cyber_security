cba68137d0ad053171916f0160dd44b342fa2857
ghofranaaa <smailikhaled841@gmail.com>
2025-04-12 16:20:22 +0100

    fixing

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois $1 | awk 'BEGIN { FS=": "; OFS="," } /^Registrant|Admin|Tech/ {print $1,$2}' > "$1.csv"
+whois "$1" | grep -E "(Registrant|Admin|Tech) (Name|Organization|Street|City|State|Postal|Country|Phone|Fax|Email)" | awk '{print $1,$2","$3,$4,$5,$6,$7,$8,$9,$10,$11,$12}' > "$1.csv"
