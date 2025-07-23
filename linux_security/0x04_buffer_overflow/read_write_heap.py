a8f841ed754e4934f5382ee0431428dd7d493557
ghofranaaa <smailikhaled841@gmail.com>
2025-07-23 18:23:52 +0100

    hack the VM - heap str injection

--- linux_security/0x04_buffer_overflow/read_write_heap.py
+++ linux_security/0x04_buffer_overflow/read_write_heap.py
@@ -57,8 +57,9 @@ def search_and_replace(pid, start, end, search_str, replace_str):
             padded = replace_str + '\x00' * (len(search_str)
                                              - len(replace_str))
             mem_file.write(padded.encode())
-            print(f"Replaced '{search_str}' with '{replace_str}'
-                  in process {pid}")
+            print(
+                f"Replaced '{search_str}' with '{replace_str}' in proc {pid}"
+            )
     except PermissionError:
         print("Error: Permission denied. Try running the script with sudo")
         sys.exit(1)
