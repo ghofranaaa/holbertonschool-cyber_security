ab1c51573ee9738712906f674d233f15201970d8
ghofranaaa <smailikhaled841@gmail.com>
2025-10-15 15:20:37 +0100

    fixed

--- scripting_cyber/0x00-ruby_scripting/7-args.rb
+++ scripting_cyber/0x00-ruby_scripting/7-args.rb
@@ -4,7 +4,9 @@ def print_arguments
 	if ARGV.empty?
 		puts "No arguments provided."
 	else
+		puts "Arguments: \n"
 		ARGV.each_with_index do |arg, index|
+
 			puts "#{index + 1}. #{arg}"
 		end
 	end
