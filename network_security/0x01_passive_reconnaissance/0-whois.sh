166f26ba5962dbda5e44fd84a4ef8bedf11aa996
ghofranaaa <smailikhaled841@gmail.com>
2026-05-20 14:26:14 +0100

    corrected

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois "$1" | awk -F': ' '/^(Registrant|Admin|Tech)/{sub(/:$/,"",$1); print $1 "," $2}' > "$1.csv"
+whois "$1" | awk -F': ' '/^(Registrant|Admin|Tech)/{sub(/:$/,"",$1); print $1 ", " $2}' > "$1.csv"
